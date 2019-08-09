9f23b62c5d1834104f3583d5c433d0e5c87c48ee
louiscklaw <louiscklaw@gmail.com>
2019-08-09 20:02:19 +0800

    update,

--- ppa_rebuild.sh
+++ ppa_rebuild.sh
@@ -4,7 +4,6 @@
 # sudo wget -O /tmp/tmp.deb http://archive.ubuntu.com/ubuntu/pool/universe/v/vlc/vlc_3.0.6-1_amd64.deb
 # sudo apt install -y /tmp/tmp.deb
 
-sudo apt install -y ack entr
 
 exit
 
@@ -18,7 +17,8 @@ sudo apt install -y wireshark
 
 # no question during installation
 
-
+sudo apt install -y ack entr
+sudo apt install -y gnome-mpv mpv
 
 sudo apt install -y gnome-shell-extension-caffeine  gnome-shell-extension-dashtodock  gnome-shell-extension-disconnect-wifi  gnome-shell-extension-hard-disk-led gnome-shell-extension-tilix-dropdown  gnome-shell-extension-hide-activities gnome-shell-extension-mediaplayer gnome-shell-extension-pixelsaver gnome-shell-extension-remove-dropdown-arrows gnome-shell-extension-trash gnome-shell-extension-weather gnome-shell-extensions gnome-shell-pomodoro
 
